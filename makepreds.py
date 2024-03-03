f5456fda67f376b3cf290b61814327f20c9a64ed
Parth <parthbangde123@gmail.com>
2024-03-03 14:39:10 +0530

    Pretrained model can run on cpu

--- makepreds.py
+++ makepreds.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     model = Model(args)
     model = model.to(device)
     model_dict = model.load_state_dict(
-        {k.replace('module.', ''): v for k, v in torch.load('ckpt/wsanodet_mix2.pkl').items()})
+        {k.replace('module.', ''): v for k, v in torch.load('ckpt/wsanodet_mix2.pkl', map_location=torch.device('cpu')).items()})
     
     model.eval()
     with torch.no_grad():
